303af4957fc454d12bb281012239d0965e864422
Brandon <brandon.shihabi@gmail.com>
2020-04-20 18:49:58 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+import 'package:bluebubbles/conversation_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.black,
+      ),
+    );
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        splashFactory: InkRipple.splashFactory,
+        // canvasColor: Colors.transparent,
+      ),
+      home: ConversationList(),
+    );
+  }
+}
