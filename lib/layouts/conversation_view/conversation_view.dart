124b818f51f59f46a29ed86f954da3da9f269952
zlshames <zlshames@gmail.com>
2020-06-10 17:11:31 -0400

    Adds custom nav bar

--- lib/layouts/conversation_view/conversation_view.dart
+++ lib/layouts/conversation_view/conversation_view.dart
@@ -5,22 +5,22 @@ import 'package:bluebubble_messages/blocs/message_bloc.dart';
 import 'package:bluebubble_messages/layouts/conversation_details/conversation_details.dart';
 import 'package:bluebubble_messages/layouts/conversation_view/messages_view.dart';
 import 'package:bluebubble_messages/layouts/conversation_view/text_field.dart';
-import 'package:bluebubble_messages/repository/models/handle.dart';
+import 'package:bluebubble_messages/layouts/widgets/CustomCupertinoNavBar.dart';
+import 'package:bluebubble_messages/managers/contact_manager.dart';
 import 'package:bluebubble_messages/socket_manager.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 import '../../helpers/hex_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'message_widget.dart';
 import '../../repository/models/chat.dart';
-import '../../repository/models/message.dart';
 
 class ConversationView extends StatefulWidget {
   ConversationView({
     Key key,
     @required this.chat,
     @required this.title,
-    @required this.messageBloc,
+    @required this.messageBloc
   }) : super(key: key);
 
   // final data;
@@ -33,28 +33,63 @@ class ConversationView extends StatefulWidget {
 }
 
 class _ConversationViewState extends State<ConversationView> {
-  // List<Message> _messages = <Message>[];
-
-  // String title = "";
-
-  // final animatedListKey = GlobalKey<AnimatedListState>();
+  ImageProvider contactImage;
 
   @override
   void didChangeDependencies() async {
     super.didChangeDependencies();
     SocketManager().removeChatNotification(widget.chat);
+
+    Chat chat = await widget.chat.getParticipants();
+    if (chat.participants.length == 1) {
+      Contact contact = getContact(
+          ContactManager().contacts, chat.participants.first.address);
+      if (contact != null && contact.avatar.length > 0) {
+        contactImage = MemoryImage(contact.avatar);
+        if (this.mounted) setState(() {});
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    var initials = getInitials(widget.title, " ");
     return Scaffold(
       backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
-      appBar: CupertinoNavigationBar(
+      appBar: CustomCupertinoNavBar(
         backgroundColor: HexColor('26262a').withOpacity(0.5),
-        middle: Text(
-          widget.title,
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        // padding: EdgeInsetsDirectional.only(top: 10.0),
+        middle: Column(
+          children: <Widget>[
+            Container(height: 10.0),
+            CircleAvatar(
+              radius: 20,
+              child: contactImage == null ? Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: AlignmentDirectional.topStart,
+                      colors: [HexColor('a0a4af'), HexColor('848894')],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Container(
+                    // child: Text("${widget.chat.title[0]}"),
+                    child: (initials is Icon) ? initials : Text(initials),
+                    alignment: AlignmentDirectional.center,
+                  ),
+                ) : CircleAvatar(
+                  backgroundImage: contactImage,
+                ),
+            ),
+            Container(height: 2.0),
+            Text(
+              widget.title,
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+            )
+          ]
         ),
         trailing: GestureDetector(
           child: Icon(
