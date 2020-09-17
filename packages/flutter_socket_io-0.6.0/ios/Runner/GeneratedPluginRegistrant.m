96c30ef51f8fd8f8ec4f0877f40189363c0a365d
Brandon <brandon.shihabi@gmail.com>
2020-09-16 18:05:25 -0700

    whoops wrong branch

---
+++ packages/flutter_socket_io-0.6.0/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_socket_io/FlutterSocketIoPlugin.h>)
+#import <flutter_socket_io/FlutterSocketIoPlugin.h>
+#else
+@import flutter_socket_io;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterSocketIoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSocketIoPlugin"]];
+}
+
+@end
