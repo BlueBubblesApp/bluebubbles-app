f8cc8bfedd4275a74e10c867dbe824f51f5b6ecc
Brandon <brandon.shihabi@gmail.com>
2020-10-28 14:20:32 -0700

    Fixed stupid bug

--- lib/managers/queue_manager.dart
+++ lib/managers/queue_manager.dart
@@ -39,7 +39,7 @@ abstract class QueueManager {
       await handleQueueItem(queued);
       await afterProcessing(queued, {});
     } catch (ex) {
-      debugPrint("Failed to handle queued item!");
+      debugPrint("Failed to handle queued item! " + ex.toString());
       debugPrint(StackTrace.current.toString());
     }
 
