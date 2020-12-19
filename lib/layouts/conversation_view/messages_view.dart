97449fda37d158686bfba38eb39ebac8dd575dff
zlshames <zlshames@gmail.com>
2020-12-18 23:01:01 -0500

    Improved scrolling in messgae view

--- lib/layouts/conversation_view/messages_view.dart
+++ lib/layouts/conversation_view/messages_view.dart
@@ -45,7 +45,6 @@ class MessagesViewState extends State<MessagesView>
   double timeStampOffset = 0;
   ScrollController scrollController = new ScrollController();
   bool showScrollDown = false;
-  int scrollState = -1; // -1: stopped, 0: start, 1: update
   List<int> loadedPages = [];
   CurrentChat currentChat;
 
@@ -55,35 +54,30 @@ class MessagesViewState extends State<MessagesView>
     widget.messageBloc.stream.listen(handleNewMessage);
 
     scrollController.addListener(() {
-      if (scrollController == null) return;
+      if (scrollController == null || !scrollController.hasClients) return;
+      if (showScrollDown && scrollController.offset >= 500) return;
+      if (!showScrollDown && scrollController.offset < 500) return;
 
-      if (scrollController.hasClients &&
-          scrollController.offset >= 500 &&
-          !showScrollDown) {
-        if (this.mounted)
-          setState(() {
-            showScrollDown = true;
-          });
-      } else if (scrollController.hasClients &&
-          scrollController.offset < 500 &&
-          showScrollDown) {
-        if (this.mounted)
-          setState(() {
-            showScrollDown = false;
-          });
+      if (scrollController.offset >= 500) {
+        showScrollDown = true;
+      } else {
+        showScrollDown = false;
+      }
+
+      if (this.mounted) {
+        setState(() {});
       }
     });
 
     EventDispatcher().stream.listen((Map<String, dynamic> event) {
-      if (!["refresh-messagebloc"].contains(event["type"]))
-        return;
+      if (!["refresh-messagebloc"].contains(event["type"])) return;
       if (!event["data"].containsKey("chatGuid")) return;
 
       // Handle event's that require a matching guid
       String chatGuid = event["data"]["chatGuid"];
       if (widget.chat.guid == chatGuid) {
         if (event["type"] == "refresh-messagebloc") {
-          // Clear state items 
+          // Clear state items
           noMoreLocalMessages = false;
           noMoreMessages = false;
           _messages = [];
@@ -160,7 +154,9 @@ class MessagesViewState extends State<MessagesView>
     // Skip deleted messages
     if (event.message != null && event.message.dateDeleted != null) return;
     if (!isNullOrEmpty(event.messages)) {
-      event.messages = event.messages.where((element) => element.dateDeleted == null).toList();
+      event.messages = event.messages
+          .where((element) => element.dateDeleted == null)
+          .toList();
     }
 
     if (event.type == MessageBlocEventType.insert) {
@@ -206,7 +202,9 @@ class MessagesViewState extends State<MessagesView>
         _listKey.currentState.insertItem(
           event.index != null ? event.index : 0,
           duration: isNewMessage
-              ? event.outGoing ? Duration(milliseconds: 500) : animationDuration
+              ? event.outGoing
+                  ? Duration(milliseconds: 500)
+                  : animationDuration
               : Duration(milliseconds: 0),
         );
       }
@@ -233,7 +231,8 @@ class MessagesViewState extends State<MessagesView>
       }
     } else if (event.type == MessageBlocEventType.remove) {
       for (int i = 0; i < _messages.length; i++) {
-        if (_messages[i].guid == event.remove && _listKey.currentState != null) {
+        if (_messages[i].guid == event.remove &&
+            _listKey.currentState != null) {
           _messages.removeAt(i);
           _listKey.currentState
               .removeItem(i, (context, animation) => Container());
@@ -299,139 +298,122 @@ class MessagesViewState extends State<MessagesView>
         });
       },
       child: Stack(alignment: AlignmentDirectional.bottomCenter, children: [
-        NotificationListener(
-          onNotification: (scrollNotification) {
-            if (scrollNotification is ScrollStartNotification &&
-                scrollState != 0) {
-              scrollState = 0;
-            } else if (scrollNotification is ScrollUpdateNotification &&
-                scrollState != 1) {
-              scrollState = 1;
-            } else if (scrollNotification is ScrollEndNotification &&
-                scrollState != -1) {
-              scrollState = -1;
-              setState(() {});
-            }
-
-            return true;
-          },
-          child: CustomScrollView(
-            controller: scrollController,
-            reverse: true,
-            physics: AlwaysScrollableScrollPhysics(
-                parent: CustomBouncingScrollPhysics()),
-            slivers: <Widget>[
-              // SliverToBoxAdapter(
-              //   child: TypingIndicator(
-              //     visible: currentChat.showTypingIndicator,
-              //   ),
-              // ),
-              _listKey != null
-                  ? SliverAnimatedList(
-                      initialItemCount: _messages.length + 1,
-                      key: _listKey,
-                      itemBuilder: (BuildContext context, int index,
-                          Animation<double> animation) {
-                        // Load more messages if we are at the top and we aren't alrady loading
-                        // and we have more messages to load
-                        if (index >= _messages.length) {
-                          if (!noMoreMessages &&
-                              (loader == null ||
-                                  !loader.isCompleted ||
-                                  !loadedPages.contains(_messages.length))) {
-                            loadNextChunk();
-                            return NewMessageLoader();
-                          }
-
-                          return Container();
+        CustomScrollView(
+          controller: scrollController,
+          reverse: true,
+          physics: AlwaysScrollableScrollPhysics(
+              parent: CustomBouncingScrollPhysics()),
+          slivers: <Widget>[
+            // SliverToBoxAdapter(
+            //   child: TypingIndicator(
+            //     visible: currentChat.showTypingIndicator,
+            //   ),
+            // ),
+            _listKey != null
+                ? SliverAnimatedList(
+                    initialItemCount: _messages.length + 1,
+                    key: _listKey,
+                    itemBuilder: (BuildContext context, int index,
+                        Animation<double> animation) {
+                      // Load more messages if we are at the top and we aren't alrady loading
+                      // and we have more messages to load
+                      if (index >= _messages.length) {
+                        if (!noMoreMessages &&
+                            (loader == null ||
+                                !loader.isCompleted ||
+                                !loadedPages.contains(_messages.length))) {
+                          loadNextChunk();
+                          return NewMessageLoader();
                         }
 
-                        Message olderMessage;
-                        Message newerMessage;
-                        if (index + 1 >= 0 && index + 1 < _messages.length) {
-                          olderMessage = _messages[index + 1];
-                        }
-                        if (index - 1 >= 0 && index - 1 < _messages.length) {
-                          newerMessage = _messages[index - 1];
-                        }
+                        return Container();
+                      }
 
-                        return SizeTransition(
-                          axis: Axis.vertical,
-                          sizeFactor: animation.drive(
-                              Tween(begin: 0.0, end: 1.0)
-                                  .chain(CurveTween(curve: Curves.easeInOut))),
-                          child: SlideTransition(
-                            position: animation.drive(
-                              Tween(
-                                begin: Offset(0.0, 1),
-                                end: Offset(0.0, 0.0),
-                              ).chain(
-                                CurveTween(
-                                  curve: Curves.easeInOut,
-                                ),
-                              ),
-                            ),
-                            child: FadeTransition(
-                              opacity: animation,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 5.0, right: 5.0),
-                                child: MessageWidget(
-                                  key: Key(_messages[index].guid),
-                                  offset: timeStampOffset,
-                                  message: _messages[index],
-                                  olderMessage: olderMessage,
-                                  newerMessage: newerMessage,
-                                  showHandle: widget.showHandle,
-                                  isFirstSentMessage:
-                                      widget.messageBloc.firstSentMessage ==
-                                          _messages[index].guid,
-                                  showHero: index == 0 &&
-                                      _messages[index].originalROWID == null,
-                                ),
+                      Message olderMessage;
+                      Message newerMessage;
+                      if (index + 1 >= 0 && index + 1 < _messages.length) {
+                        olderMessage = _messages[index + 1];
+                      }
+                      if (index - 1 >= 0 && index - 1 < _messages.length) {
+                        newerMessage = _messages[index - 1];
+                      }
+
+                      return SizeTransition(
+                        axis: Axis.vertical,
+                        sizeFactor: animation.drive(Tween(begin: 0.0, end: 1.0)
+                            .chain(CurveTween(curve: Curves.easeInOut))),
+                        child: SlideTransition(
+                          position: animation.drive(
+                            Tween(
+                              begin: Offset(0.0, 1),
+                              end: Offset(0.0, 0.0),
+                            ).chain(
+                              CurveTween(
+                                curve: Curves.easeInOut,
                               ),
                             ),
                           ),
+                          child: FadeTransition(
+                            opacity: animation,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 5.0, right: 5.0),
+                              child: MessageWidget(
+                                key: Key(_messages[index].guid),
+                                offset: timeStampOffset,
+                                message: _messages[index],
+                                olderMessage: olderMessage,
+                                newerMessage: newerMessage,
+                                showHandle: widget.showHandle,
+                                isFirstSentMessage:
+                                    widget.messageBloc.firstSentMessage ==
+                                        _messages[index].guid,
+                                showHero: index == 0 &&
+                                    _messages[index].originalROWID == null,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                : SliverToBoxAdapter(child: Container()),
+            SliverPadding(
+              padding: EdgeInsets.all(70),
+            ),
+          ],
+        ),
+        AnimatedOpacity(
+            opacity: showScrollDown ? 1 : 0,
+            duration: new Duration(milliseconds: 300),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  height: 35,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        scrollController.animateTo(
+                          0.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 300),
                         );
                       },
-                    )
-                  : SliverToBoxAdapter(child: Container()),
-              SliverPadding(
-                padding: EdgeInsets.all(70),
-              ),
-            ],
-          ),
-        ),
-        (showScrollDown && scrollState == -1)
-            ? ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                  child: Container(
-                    height: 35,
-                    width: 150,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).accentColor.withOpacity(0.7),
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Center(
-                      child: GestureDetector(
-                        onTap: () {
-                          scrollController.animateTo(
-                            0.0,
-                            curve: Curves.easeOut,
-                            duration: const Duration(milliseconds: 300),
-                          );
-                        },
-                        child: Text(
-                          "\u{2193} Scroll to bottom \u{2193}",
-                          textAlign: TextAlign.center,
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
+                      child: Text(
+                        "\u{2193} Scroll to bottom \u{2193}",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
                     ),
                   ),
                 ),
-              )
-            : Container()
+              ),
+            ))
       ]),
     );
   }
