f51c4435720158975e669657a25742c5a1a4ed8d
zlshames <zlshames@gmail.com>
2020-12-22 17:07:36 -0500

    Try catch wrap

--- lib/helpers/attachment_downloader.dart
+++ lib/helpers/attachment_downloader.dart
@@ -103,9 +103,13 @@ class AttachmentDownloader {
       debugPrint(
           "Attachment downloaded in ${stopwatch.elapsedMilliseconds} ms");
 
-      // Get the dimensions of the attachment
-      await AttachmentHelper.setDimensions(attachment);
-      await attachment.update();
+      try {
+        // Get the dimensions of the attachment
+        await AttachmentHelper.setDimensions(attachment);
+        await attachment.update();
+      } catch (ex) {
+        // So what if it crashes here.... I don't care...
+      }
 
       File file = new File(attachment.getPath());
 
