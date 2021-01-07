a1db8005e6a1033244afda23920de9ebb266d323
zlshames <zlshames@gmail.com>
2021-01-06 22:28:20 -0500

    Adds search cache and no results text

--- lib/layouts/search/search_view.dart
+++ lib/layouts/search/search_view.dart
@@ -34,6 +34,9 @@ class SearchViewState extends State<SearchView> with TickerProviderStateMixin {
   bool isSearching = false;
   Map<String, Chat> chatCache = {};
   FocusNode _focusNode = new FocusNode();
+  Map<String, int> resultCache = {};
+  bool noResults = false;
+  String previousSearch;
 
   @override
   void initState() {
@@ -45,6 +48,17 @@ class SearchViewState extends State<SearchView> with TickerProviderStateMixin {
       if (event["type"] == 'theme-update' && this.mounted) {
         setState(() {
           gotBrightness = false;
+        });
+      }
+    });
+
+    // When the user types again after no results, reset no results
+    textEditingController.addListener(() {
+      if (textEditingController.text != previousSearch &&
+          noResults &&
+          this.mounted) {
+        setState(() {
+          noResults = false;
         });
       }
     });
@@ -74,6 +88,19 @@ class SearchViewState extends State<SearchView> with TickerProviderStateMixin {
   Future<void> search(String term) async {
     if (isSearching || isNullOrEmpty(term) || term.length < 3) return;
     _focusNode.unfocus();
+    noResults = false;
+    previousSearch = term;
+
+    // If we've already searched for the results and there are none, set no results and return
+    if (resultCache.containsKey(term) &&
+        resultCache[term] == 0 &&
+        this.mounted) {
+      setState(() {
+        noResults = true;
+      });
+
+      return;
+    }
 
     if (this.mounted)
       setState(() {
@@ -125,10 +152,14 @@ class SearchViewState extends State<SearchView> with TickerProviderStateMixin {
     // Let the animated list know it should update
     _listKey?.currentState?.setState(() {});
 
+    // Add the cached result
+    resultCache[term] = this.results.length;
+
     // Update the UI with the results
     if (this.mounted) {
       setState(() {
         isSearching = false;
+        noResults = this.results.length == 0;
       });
     }
   }
@@ -227,123 +258,148 @@ class SearchViewState extends State<SearchView> with TickerProviderStateMixin {
                             )
                     ])),
             Divider(color: Theme.of(context).accentColor),
-            Flexible(
-              fit: FlexFit.loose,
-              child: AnimatedList(
-                key: _listKey,
-                initialItemCount: results.length,
-                itemBuilder: (BuildContext context, int index,
-                    Animation<double> animation) {
-                  print(this.results[index]);
-                  Message message = results[index]['message'];
-                  Chat chat = results[index]['chat'];
+            (!this.isSearching && noResults)
+                ? Padding(
+                    padding: EdgeInsets.only(top: 25.0),
+                    child: Text("No results found!",
+                        style: Theme.of(context).textTheme.bodyText1))
+                : (!this.isSearching)
+                    ? Flexible(
+                        fit: FlexFit.loose,
+                        child: AnimatedList(
+                          key: _listKey,
+                          initialItemCount: results.length,
+                          itemBuilder: (BuildContext context, int index,
+                              Animation<double> animation) {
+                            Message message = results[index]['message'];
+                            Chat chat = results[index]['chat'];
 
-                  // Create the textspans
-                  List<InlineSpan> spans = [];
+                            // Create the textspans
+                            List<InlineSpan> spans = [];
 
-                  // Get the current position of the search term
-                  int termIndex = (message.text ?? "")
-                      .toLowerCase()
-                      .indexOf(textEditingController.text.toLowerCase());
-                  int termEnd = termIndex + textEditingController.text.length;
+                            // Get the current position of the search term
+                            int termIndex = (message.text ?? "")
+                                .toLowerCase()
+                                .indexOf(
+                                    textEditingController.text.toLowerCase());
+                            int termEnd =
+                                termIndex + textEditingController.text.length;
 
-                  if (termIndex >= 0) {
-                    // We only want a snippet of the text, so only get a 50x50 range
-                    // of characters from the string, with the search term in the middle
-                    String subText = message.text.substring(
-                        (termIndex - 50 >= 0) ? termIndex - 50 : 0,
-                        (termEnd + 50 < message.text.length)
-                            ? termEnd + 50
-                            : message.text.length);
+                            if (termIndex >= 0) {
+                              // We only want a snippet of the text, so only get a 50x50 range
+                              // of characters from the string, with the search term in the middle
+                              String subText = message.text.substring(
+                                  (termIndex - 50 >= 0) ? termIndex - 50 : 0,
+                                  (termEnd + 50 < message.text.length)
+                                      ? termEnd + 50
+                                      : message.text.length);
 
-                    // Recarculate the term position in the snippet
-                    termIndex = subText
-                        .toLowerCase()
-                        .indexOf(textEditingController.text.toLowerCase());
-                    termEnd = termIndex + textEditingController.text.length;
+                              // Recarculate the term position in the snippet
+                              termIndex = subText.toLowerCase().indexOf(
+                                  textEditingController.text.toLowerCase());
+                              termEnd =
+                                  termIndex + textEditingController.text.length;
 
-                    // Add the beginning string
-                    spans.add(TextSpan(
-                        text: subText.substring(0, termIndex).trimLeft(),
-                        style: Theme.of(context).textTheme.subtitle1));
+                              // Add the beginning string
+                              spans.add(TextSpan(
+                                  text: subText
+                                      .substring(0, termIndex)
+                                      .trimLeft(),
+                                  style:
+                                      Theme.of(context).textTheme.subtitle1));
 
-                    // Add the search term
-                    spans.add(TextSpan(
-                        text: subText.substring(termIndex, termEnd),
-                        style: Theme.of(context).textTheme.subtitle1.apply(
-                            color: Theme.of(context).primaryColor,
-                            fontWeightDelta: 2)));
+                              // Add the search term
+                              spans.add(TextSpan(
+                                  text: subText.substring(termIndex, termEnd),
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .subtitle1
+                                      .apply(
+                                          color: Theme.of(context).primaryColor,
+                                          fontWeightDelta: 2)));
 
-                    // Add the ending string
-                    spans.add(TextSpan(
-                        text: subText
-                            .substring(termEnd, subText.length)
-                            .trimRight(),
-                        style: Theme.of(context).textTheme.subtitle1));
-                  } else {
-                    spans.add(TextSpan(
-                        text: message.text,
-                        style: Theme.of(context).textTheme.subtitle1));
-                  }
+                              // Add the ending string
+                              spans.add(TextSpan(
+                                  text: subText
+                                      .substring(termEnd, subText.length)
+                                      .trimRight(),
+                                  style:
+                                      Theme.of(context).textTheme.subtitle1));
+                            } else {
+                              spans.add(TextSpan(
+                                  text: message.text,
+                                  style:
+                                      Theme.of(context).textTheme.subtitle1));
+                            }
 
-                  return Column(
-                    key: new Key("result-${message.guid}"),
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      ListTile(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            CupertinoPageRoute(
-                              builder: (BuildContext context) {
-                                MessageBloc customBloc =
-                                    new MessageBloc(chat, canLoadMore: false);
+                            return Column(
+                              key: new Key("result-${message.guid}"),
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      CupertinoPageRoute(
+                                        builder: (BuildContext context) {
+                                          MessageBloc customBloc =
+                                              new MessageBloc(chat,
+                                                  canLoadMore: false);
 
-                                return ConversationView(
-                                  chat: chat,
-                                  existingAttachments: [],
-                                  existingText: null,
-                                  isCreator: false,
-                                  customMessageBloc: customBloc,
-                                  onMessagesViewComplete: () {
-                                    customBloc.loadSearchChunk(message);
+                                          return ConversationView(
+                                            chat: chat,
+                                            existingAttachments: [],
+                                            existingText: null,
+                                            isCreator: false,
+                                            customMessageBloc: customBloc,
+                                            onMessagesViewComplete: () {
+                                              customBloc
+                                                  .loadSearchChunk(message);
+                                            },
+                                          );
+                                        },
+                                      ),
+                                    );
                                   },
-                                );
-                              },
-                            ),
-                          );
-                        },
-                        dense: true,
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("${dateToShortString(message.dateCreated)}",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .subtitle1
-                                    .apply(fontSizeDelta: -2)),
-                            Container(height: 5.0),
-                            Text(chat?.title,
-                                style: Theme.of(context).textTheme.bodyText1),
-                          ],
+                                  dense: true,
+                                  title: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          "${dateToShortString(message.dateCreated)}",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle1
+                                              .apply(fontSizeDelta: -2)),
+                                      Container(height: 5.0),
+                                      Text(chat?.title,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1),
+                                    ],
+                                  ),
+                                  subtitle: Padding(
+                                    padding: EdgeInsets.only(top: 5.0),
+                                    child: RichText(
+                                      text: TextSpan(children: spans),
+                                    ),
+                                  ),
+                                  trailing: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1
+                                        .color,
+                                  ),
+                                ),
+                                Divider(color: Theme.of(context).accentColor)
+                              ],
+                            );
+                          },
                         ),
-                        subtitle: Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: RichText(
-                            text: TextSpan(children: spans),
-                          ),
-                        ),
-                        trailing: Icon(
-                          Icons.arrow_forward_ios,
-                          color: Theme.of(context).textTheme.bodyText1.color,
-                        ),
-                      ),
-                      Divider(color: Theme.of(context).accentColor)
-                    ],
-                  );
-                },
-              ),
-            )
+                      )
+                    : Container()
           ],
         ),
       ),
