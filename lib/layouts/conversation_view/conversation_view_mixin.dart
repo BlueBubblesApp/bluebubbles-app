de108b35f8333e629775a81f2da04080da4d8a8d
tanay <tanay@neotia.in>
2021-07-08 17:58:46 -0400

    Fix ">" text not showing up on long group names

--- lib/layouts/conversation_view/conversation_view_mixin.dart
+++ lib/layouts/conversation_view/conversation_view_mixin.dart
@@ -538,23 +538,47 @@ mixin ConversationViewMixin<ConversationViewState extends StatefulWidget> on Sta
                       alignment: Alignment.center,
                     ),
                     Container(height: 5.0),
-                    RichText(
-                      maxLines: 1,
-                      overflow: Cupertino.TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        style: Theme.of(context).textTheme.headline2,
-                        children: [
-                          TextSpan(
-                            text: title,
-                            style: titleStyle,
-                          ),
-                          TextSpan(
-                            text: " >",
-                            style: Theme.of(context).textTheme.subtitle1,
-                          ),
-                        ],
-                      ),
+                    Center(
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: Get.width / 2,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              constraints: BoxConstraints(
+                                maxWidth: Get.width / 2 - 55,
+                              ),
+                              child: RichText(
+                                maxLines: 1,
+                                overflow: Cupertino.TextOverflow.ellipsis,
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                  style: Theme.of(context).textTheme.headline2,
+                                  children: [
+                                    TextSpan(
+                                      text: title,
+                                      style: titleStyle,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                style: Theme.of(context).textTheme.headline2,
+                                children: [
+                                  TextSpan(
+                                    text: " >",
+                                    style: Theme.of(context).textTheme.subtitle1,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ]
+                        ),
+                      )
                     ),
                   ],
                 ),
