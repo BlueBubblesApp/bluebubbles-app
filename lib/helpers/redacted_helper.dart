ddbfc8f5eba3e808fa67b7f3ed474efbb6bc7c2d
Joel Jothiprakasam <hijoelj@gmail.com>
2021-08-03 11:47:05 -0700

    Fix gray screen in conversation details

--- lib/helpers/redacted_helper.dart
+++ lib/helpers/redacted_helper.dart
@@ -3,19 +3,19 @@ import 'package:bluebubbles/managers/settings_manager.dart';
 import 'package:bluebubbles/repository/models/chat.dart';
 import 'package:flutter/widgets.dart';
 
-String? getContactName(BuildContext context, String? contactTitle, String? contactAddress, {Chat? currentChat}) {
+String getContactName(BuildContext context, String? contactTitle, String? contactAddress, {Chat? currentChat}) {
   final bool redactedMode = SettingsManager().settings.redactedMode.value;
   final bool hideInfo = redactedMode && SettingsManager().settings.hideContactInfo.value;
   final bool generateName = redactedMode && SettingsManager().settings.generateFakeContactNames.value;
 
-  String? contactName = contactTitle;
-  if (hideInfo) {
+  String contactName = contactTitle ?? "";
+  if (hideInfo || generateName) {
     currentChat = CurrentChat.of(context)?.chat ?? currentChat;
     int index = (currentChat?.participants ?? []).indexWhere((h) => h.address == contactAddress);
     List<String?> fakeNames = currentChat?.fakeParticipants ?? [];
     if (generateName && fakeNames.isNotEmpty) {
       if (index >= 0 && index < fakeNames.length) {
-        contactName = currentChat?.fakeParticipants[index];
+        contactName = currentChat?.fakeParticipants[index] ?? "Fake Name";
       }
     } else if (generateName) {
       contactName = "Fake Name";
