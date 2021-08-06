95a4591f481a69edf6fa28b9e277fd150ae2b20b
tanay <tanay@neotia.in>
2021-08-05 22:03:16 -0400

    Small change

--- lib/layouts/widgets/message_widget/message_content/media_players/image_widget.dart
+++ lib/layouts/widgets/message_widget/message_content/media_players/image_widget.dart
@@ -65,7 +65,7 @@ class _ImageWidgetState extends State<ImageWidget> with TickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    _initializeBytes();
+    Future.delayed(Duration.zero, () => _initializeBytes());
 
     return VisibilityDetector(
       key: Key(widget.attachment.guid!),
