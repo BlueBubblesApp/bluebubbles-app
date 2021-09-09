529c0829a436897d1c11a0c1d9f37e63eeaee7e4
Joel Jothiprakasam <hijoelj@gmail.com>
2021-09-08 22:51:24 -0500

    Revert commenting out an oncontentcommit
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/layouts/conversation_view/text_field/blue_bubbles_text_field.dart
+++ lib/layouts/conversation_view/text_field/blue_bubbles_text_field.dart
@@ -696,7 +696,7 @@ class BlueBubblesTextFieldState extends State<BlueBubblesTextField> with TickerP
                               : Colors.white,
                           fontSizeDelta: -0.25,
                         ),
-                    // onContentCommitted: onContentCommit,
+                    onContentCommitted: onContentCommit,
                     decoration: InputDecoration(
                       isDense: true,
                       enabledBorder: OutlineInputBorder(
