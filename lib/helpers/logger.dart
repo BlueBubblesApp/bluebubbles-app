b5c8b02389b772d15940e0c695d1a318d69e0680
Joel Jothiprakasam <hijoelj@gmail.com>
2021-09-17 11:32:20 -0500

    add import
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/helpers/logger.dart
+++ lib/helpers/logger.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
-import 'package:path_provider/path_provider.dart';
-import 'package:universal_html/html.dart' as html;
-
-import 'package:flutter/foundation.dart';
-import 'package:universal_io/io.dart';
 
 import 'package:bluebubbles/helpers/share.dart';
 import 'package:bluebubbles/helpers/utils.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:universal_html/html.dart' as html;
+import 'package:universal_io/io.dart';
 
 // ignore: non_constant_identifier_names
 BaseLogger Logger = Get.isRegistered<BaseLogger>() ? Get.find<BaseLogger>() : Get.put(BaseLogger());
