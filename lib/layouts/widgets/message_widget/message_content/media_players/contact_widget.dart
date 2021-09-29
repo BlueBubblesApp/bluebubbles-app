56e8af0a02306c68f3ba990855d9e3b375e3dbcc
Tanay Neotia <tanay@neotia.in>
2021-09-29 14:11:50 -0400

    Few more minor things

--- lib/layouts/widgets/message_widget/message_content/media_players/contact_widget.dart
+++ lib/layouts/widgets/message_widget/message_content/media_players/contact_widget.dart
@@ -35,13 +35,16 @@ class _ContactWidgetState extends State<ContactWidget> {
   @override
   void initState() {
     super.initState();
+    init();
+  }
 
+  void init() async {
     String appleContact;
 
     if (kIsWeb || widget.file.path == null) {
       appleContact = utf8.decode(widget.file.bytes!);
     } else {
-      appleContact = File(widget.file.path!).readAsStringSync();
+      appleContact = await File(widget.file.path!).readAsString();
     }
 
     try {
@@ -50,7 +53,7 @@ class _ContactWidgetState extends State<ContactWidget> {
       contact = Contact(displayName: "Invalid Contact", id: randomString(8));
     }
 
-    if (mounted) setState(() {});
+    if (!kIsWeb && widget.file.path != null && mounted) setState(() {});
   }
 
   @override
