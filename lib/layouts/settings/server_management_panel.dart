1d62ea2c2381c96049a7cc1fe97da8251a0966c0
Tanay Neotia <tanay@neotia.in>
2021-10-05 16:29:07 -0400

    Potentially fix chat snapping in & make message sync fetch max 1000 messages

--- lib/layouts/settings/server_management_panel.dart
+++ lib/layouts/settings/server_management_panel.dart
@@ -889,7 +889,7 @@ class _SyncDialogState extends State<SyncDialog> {
     if (lookback == null) return;
 
     DateTime now = DateTime.now().toUtc().subtract(lookback!);
-    SocketManager().fetchMessages(null, after: now.millisecondsSinceEpoch)!.then((dynamic messages) {
+    SocketManager().fetchMessages(null, after: now.millisecondsSinceEpoch, limit: 1000)!.then((dynamic messages) {
       if (mounted) {
         setState(() {
           message = "Adding ${messages.length} messages...";
