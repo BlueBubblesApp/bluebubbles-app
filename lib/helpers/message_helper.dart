6cedeab8182720081a66bca9cafff87c2f3f15f1
Tanay Neotia <tanay@neotia.in>
2021-11-11 14:03:23 -0500

    Fix reaction message text

--- lib/helpers/message_helper.dart
+++ lib/helpers/message_helper.dart
@@ -303,9 +303,9 @@ class MessageHelper {
         if (associatedMessage.isInteractive()) {
           return "$sender $verb ${getInteractiveText(associatedMessage)}";
         // now we check if theres a subject or text and construct out message based off that
-        } else if (!isNullOrEmpty(associatedMessage.text, trimString: true)!
+        } else if (!isNullOrEmpty(associatedMessage.subject, trimString: true)!
             || !isNullOrEmpty(associatedMessage.text, trimString: true)!) {
-          String messageText = "${associatedMessage.subject} ${associatedMessage.text}";
+          String messageText = (associatedMessage.subject ?? "") + (associatedMessage.text ?? "");
           return '$sender $verb “${messageText.trim()}”';
         // if it has an attachment, we should fetch the attachments and get the attachment text
         } else if (associatedMessage.hasAttachments) {
