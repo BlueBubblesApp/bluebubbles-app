cc3d3dbfde9c3bbda1df1bdc5a5f19ceef3be8a4
Tanay Neotia <tanay@neotia.in>
2021-11-18 10:47:11 -0500

    #1718

--- lib/repository/models/io/chat.dart
+++ lib/repository/models/io/chat.dart
@@ -178,7 +178,7 @@ Future<List<Message>> getMessagesIsolate(List<dynamic> stuff) async {
       Message message = messages[i];
       if (handles.isNotEmpty && message.handleId != 0) {
         Handle? handle = handles.firstWhereOrNull((e) => e?.id == message.handleId);
-        if (handle == null) {
+        if (handle == null && message.originalROWID != null) {
           messages.remove(message);
           i--;
         } else {
