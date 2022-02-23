3bc42f0db04f55363f382a1942500f53e8cde60d
Tanay Neotia <tanay@neotia.in>
2022-02-22 19:25:57 -0500

    #1881

--- lib/layouts/conversation_details/conversation_details.dart
+++ lib/layouts/conversation_details/conversation_details.dart
@@ -376,7 +376,7 @@ class _ConversationDetailsState extends State<ConversationDetails> with WidgetsB
                   SliverPadding(
                     padding: EdgeInsets.symmetric(vertical: 10),
                   ),
-                  if (SettingsManager().settings.enablePrivateAPI.value && chat.isIMessage)
+                  if (SettingsManager().settings.enablePrivateAPI.value && chat.isIMessage && chat.isGroup())
                     SliverToBoxAdapter(
                       child: Padding(
                           padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8.0),
