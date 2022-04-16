db41cbbd802218cc86d18e7b521e08c121288b07
Tanay Neotia <tanay@neotia.in>
2022-04-16 19:19:59 -0400

    Add contact avatars to web notifications

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -799,14 +799,16 @@ Future<void> paintGroupAvatar({
   required Canvas canvas,
   required double size,
 }) async {
-  String customPath = join((await getApplicationSupportDirectory()).path, "avatars",
-      chatGuid.characters.where((c) => c.isAlphabetOnly || c.isNum).join(), "avatar.jpg");
+  if (kIsDesktop) {
+    String customPath = join((await getApplicationSupportDirectory()).path, "avatars",
+        chatGuid.characters.where((c) => c.isAlphabetOnly || c.isNum).join(), "avatar.jpg");
 
-  if (File(customPath).existsSync()) {
-    Uint8List? customAvatar = await circularize(File(customPath).readAsBytesSync(), size: size.toInt());
-    if (customAvatar != null) {
-      canvas.drawImage(await loadImage(customAvatar), Offset(0, 0), Paint());
-      return;
+    if (File(customPath).existsSync()) {
+      Uint8List? customAvatar = await circularize(File(customPath).readAsBytesSync(), size: size.toInt());
+      if (customAvatar != null) {
+        canvas.drawImage(await loadImage(customAvatar), Offset(0, 0), Paint());
+        return;
+      }
     }
   }
 
@@ -888,24 +890,26 @@ Future<void> paintAvatar(
   fontSize ??= size * 0.5;
   borderWidth ??= size * 0.05;
 
-  String customPath = join((await getApplicationSupportDirectory()).path, "avatars",
-      chatGuid.characters.where((c) => c.isAlphabetOnly || c.isNum).join(), "avatar.jpg");
+  if (kIsDesktop) {
+    String customPath = join((await getApplicationSupportDirectory()).path, "avatars",
+        chatGuid.characters.where((c) => c.isAlphabetOnly || c.isNum).join(), "avatar.jpg");
 
-  if (File(customPath).existsSync()) {
-    Uint8List? customAvatar = await circularize(File(customPath).readAsBytesSync(), size: size.toInt());
-    if (customAvatar != null) {
-      canvas.drawImage(await loadImage(customAvatar), offset, Paint());
-      return;
-    }
-  } else {
-    Contact? contact = ContactManager().getContact(handle?.address);
-    if (contact?.hasAvatar ?? false) {
-      Uint8List? contactAvatar =
-          await circularize(contact!.avatarHiRes.value ?? contact.avatar.value!, size: size.toInt());
-      if (contactAvatar != null) {
-        canvas.drawImage(await loadImage(contactAvatar), offset, Paint());
+    if (File(customPath).existsSync()) {
+      Uint8List? customAvatar = await circularize(File(customPath).readAsBytesSync(), size: size.toInt());
+      if (customAvatar != null) {
+        canvas.drawImage(await loadImage(customAvatar), offset, Paint());
         return;
       }
+    }
+  }
+
+  Contact? contact = ContactManager().getContact(handle?.address);
+  if (contact?.hasAvatar ?? false) {
+    Uint8List? contactAvatar =
+        await circularize(contact!.avatarHiRes.value ?? contact.avatar.value!, size: size.toInt());
+    if (contactAvatar != null) {
+      canvas.drawImage(await loadImage(contactAvatar), offset, Paint());
+      return;
     }
   }
 
