bb329e78d2cc13ef6e71ee9ff9fe9ce1904622a6
Tanay Neotia <tanay@neotia.in>
2022-07-23 23:07:08 -0400

    Import fix

--- lib/layouts/wrappers/titlebar_wrapper.dart
+++ lib/layouts/wrappers/titlebar_wrapper.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:bluebubbles/helpers/utils.dart';
+import 'package:bluebubbles/helpers/window_effects.dart';
 import 'package:bluebubbles/managers/settings_manager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
