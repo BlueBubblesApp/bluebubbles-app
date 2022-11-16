0eefe43562ff622790251f12a59e7c654605282f
Tanay Neotia <tanay@neotia.in>
2022-11-16 15:39:46 -0500

    Add message sender text & icon back

--- lib/app/layouts/conversation_view/widgets/message/message_holder.dart
+++ lib/app/layouts/conversation_view/widgets/message/message_holder.dart
@@ -3,12 +3,14 @@ import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/attach
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/chat_event/chat_event.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/interactive/interactive_holder.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/misc/message_properties.dart';
+import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/misc/message_sender.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/misc/slide_to_reply.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/reaction/reaction_holder.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/text/text_bubble.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/timestamp/delivered_indicator.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/timestamp/message_timestamp.dart';
 import 'package:bluebubbles/app/layouts/conversation_view/widgets/message/timestamp/timestamp_separator.dart';
+import 'package:bluebubbles/app/widgets/avatars/contact_avatar_widget.dart';
 import 'package:bluebubbles/app/wrappers/stateful_boilerplate.dart';
 import 'package:bluebubbles/helpers/helpers.dart';
 import 'package:bluebubbles/models/models.dart';
@@ -42,6 +44,8 @@ class _MessageHolderState extends CustomState<MessageHolder, void, MessageWidget
   Message? get newerMessage => controller.newMwc?.message;
   Chat get chat => widget.cvController.chat;
   bool get canSwipeToReply => ss.settings.enablePrivateAPI.value && ss.settings.swipeToReply.value && chat.isIMessage;
+  bool get showSender => !message.isGroupEvent && (!message.sameSender(olderMessage)
+      || (olderMessage == null || !message.dateCreated!.isWithin(olderMessage!.dateCreated!, minutes: 30)));
 
   List<MessagePart> messageParts = [];
   List<RxDouble> replyOffsets = [];
@@ -141,106 +145,159 @@ class _MessageHolderState extends CustomState<MessageHolder, void, MessageWidget
   Widget build(BuildContext context) {
     final stickers = message.associatedMessages.where((e) => e.associatedMessageType == "sticker");
     final reactions = message.associatedMessages.where((e) => ReactionTypes.toList().contains(e.associatedMessageType));
+    /// Layout tree
+    /// - Timestamp
+    /// - Stack (see code comment)
+    ///    - avatar | message row
+    ///                - spacing (for avatar) | message column | message timestamp
+    ///                                          - message part column
+    ///                                             - message sender
+    ///                                             - reaction spacing box
+    ///                                             - message content row
+    ///                                                - text / attachment / chat event / interactive | slide to reply
+    ///                                                   |-> stack: stickers & reactions
+    ///                                             - message properties
+    ///                                          - delivered indicator
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        // large timestamp between messages
         TimestampSeparator(olderMessage: olderMessage, message: message),
-        Row(
+        // use stack so avatar can be placed at bottom
+        Stack(
+          alignment: Alignment.bottomLeft,
+          clipBehavior: Clip.none,
           children: [
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: message.isFromMe! ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                children: [
-                  ...messageParts.mapIndexed((index, e) => Column(
+            // avatar, if needed
+            if (message.showTail(newerMessage) && (chat.isGroup || ss.settings.alwaysShowAvatars.value) && !message.isFromMe!)
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: ContactAvatarWidget(
+                  handle: message.handle,
+                  size: 30,
+                  fontSize: context.theme.textTheme.bodyLarge!.fontSize!,
+                  borderThickness: 0.1,
+                ),
+              ),
+            Row(
+              children: [
+                // shift over by avatar width
+                if (chat.isGroup)
+                  const SizedBox(width: 35),
+                Expanded(
+                  child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: message.isFromMe! ? CrossAxisAlignment.end : CrossAxisAlignment.start,
                     children: [
-                      Row(
+                      // message column
+                      ...messageParts.mapIndexed((index, e) => Column(
                         mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: message.isFromMe! ? CrossAxisAlignment.end : CrossAxisAlignment.start,
                         children: [
-                          if (message.isGroupEvent || e.isUnsent)
-                            ChatEvent(
-                              part: e,
-                              message: message,
-                            ),
-                          if (!message.isGroupEvent && !e.isUnsent)
-                            Stack(
-                              alignment: Alignment.center,
-                              fit: StackFit.loose,
-                              clipBehavior: Clip.none,
-                              children: [
-                                GestureDetector(
-                                  behavior: HitTestBehavior.deferToChild,
-                                  // todo onTap: kIsDesktop || kIsWeb ? () => tapped.value = !tapped.value : null,
-                                  onHorizontalDragUpdate: !canSwipeToReply ? null : (details) {
-                                    if ((message.isFromMe! && details.delta.dx > 0) || (!message.isFromMe! && details.delta.dx < 0)) {
-                                      return;
-                                    }
-                                    final offset = replyOffsets[index];
-                                    offset.value += details.delta.dx * 0.5;
-                                    if (!gaveHapticFeedback && offset.value.abs() >= SlideToReply.replyThreshold) {
-                                      HapticFeedback.lightImpact();
-                                      gaveHapticFeedback = true;
-                                    } else if (offset.value.abs() < SlideToReply.replyThreshold) {
-                                      gaveHapticFeedback = false;
-                                    }
-                                  },
-                                  onHorizontalDragEnd: !canSwipeToReply ? null : (details) {
-                                    final offset = replyOffsets[index];
-                                    if (offset.value.abs() >= SlideToReply.replyThreshold) {
-                                      widget.cvController.replyToMessage = message;
-                                    }
-                                    offset.value = 0;
-                                  },
-                                  onHorizontalDragCancel: !canSwipeToReply ? null : () {
-                                    replyOffsets[index].value = 0;
-                                  },
-                                  child: message.hasApplePayloadData || message.isLegacyUrlPreview || message.isInteractive ? InteractiveHolder(
-                                    parentController: controller,
-                                    message: e,
-                                  ) : e.attachments.isEmpty ? TextBubble(
-                                    parentController: controller,
-                                    message: e,
-                                  ) : AttachmentHolder(
-                                    parentController: controller,
-                                    message: e,
-                                  ),
+                          // show sender, if needed
+                          if (chat.isGroup && !message.isFromMe! && showSender && e.part == (messageParts.firstWhereOrNull((e) => !e.isUnsent)?.part))
+                            MessageSender(olderMessage: olderMessage, message: message),
+                          // add a box to account for height of reactions if
+                          // a sender wasn't added and there are reactions
+                          if (reactions.where((s) => (s.associatedMessagePart ?? 0) == e.part).isNotEmpty
+                              && !(chat.isGroup && !message.isFromMe! && showSender && e.part == messageParts.firstWhereOrNull((e) => !e.isUnsent)?.part))
+                            const SizedBox(height: 15),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              // show group event
+                              if (message.isGroupEvent || e.isUnsent)
+                                ChatEvent(
+                                  part: e,
+                                  message: message,
                                 ),
-                                StickerHolder(stickerMessages: stickers.where((s) => (s.associatedMessagePart ?? 0) == e.part)),
-                                if (message.isFromMe!)
-                                  Positioned(
-                                    top: -15,
-                                    left: -20,
-                                    child: ReactionHolder(
-                                      reactions: reactions.where((s) => (s.associatedMessagePart ?? 0) == e.part),
-                                      message: message,
+                              // otherwise show content
+                              if (!message.isGroupEvent && !e.isUnsent)
+                                Stack(
+                                  alignment: Alignment.center,
+                                  fit: StackFit.loose,
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    // actual message content
+                                    GestureDetector(
+                                      behavior: HitTestBehavior.deferToChild,
+                                      // todo onTap: kIsDesktop || kIsWeb ? () => tapped.value = !tapped.value : null,
+                                      onHorizontalDragUpdate: !canSwipeToReply ? null : (details) {
+                                        if ((message.isFromMe! && details.delta.dx > 0) || (!message.isFromMe! && details.delta.dx < 0)) {
+                                          return;
+                                        }
+                                        final offset = replyOffsets[index];
+                                        offset.value += details.delta.dx * 0.5;
+                                        if (!gaveHapticFeedback && offset.value.abs() >= SlideToReply.replyThreshold) {
+                                          HapticFeedback.lightImpact();
+                                          gaveHapticFeedback = true;
+                                        } else if (offset.value.abs() < SlideToReply.replyThreshold) {
+                                          gaveHapticFeedback = false;
+                                        }
+                                      },
+                                      onHorizontalDragEnd: !canSwipeToReply ? null : (details) {
+                                        final offset = replyOffsets[index];
+                                        if (offset.value.abs() >= SlideToReply.replyThreshold) {
+                                          widget.cvController.replyToMessage = message;
+                                        }
+                                        offset.value = 0;
+                                      },
+                                      onHorizontalDragCancel: !canSwipeToReply ? null : () {
+                                        replyOffsets[index].value = 0;
+                                      },
+                                      child: message.hasApplePayloadData || message.isLegacyUrlPreview || message.isInteractive ? InteractiveHolder(
+                                        parentController: controller,
+                                        message: e,
+                                      ) : e.attachments.isEmpty ? TextBubble(
+                                        parentController: controller,
+                                        message: e,
+                                      ) : AttachmentHolder(
+                                        parentController: controller,
+                                        message: e,
+                                      ),
                                     ),
-                                  ),
-                                if (!message.isFromMe!)
-                                  Positioned(
-                                    top: -15,
-                                    right: -20,
-                                    child: ReactionHolder(
-                                      reactions: reactions.where((s) => (s.associatedMessagePart ?? 0) == e.part),
-                                      message: message,
-                                    ),
-                                  ),
-                              ],
-                            ),
-                          if (canSwipeToReply && !message.isGroupEvent && !e.isUnsent)
-                            Obx(() => SlideToReply(width: replyOffsets[index].value.abs())),
-                        ].conditionalReverse(message.isFromMe!),
-                      ),
-                      MessageProperties(parentController: controller, part: e),
+                                    // show stickers on top
+                                    StickerHolder(stickerMessages: stickers.where((s) => (s.associatedMessagePart ?? 0) == e.part)),
+                                    // show reactions on top
+                                    if (message.isFromMe!)
+                                      Positioned(
+                                        top: -15,
+                                        left: -20,
+                                        child: ReactionHolder(
+                                          reactions: reactions.where((s) => (s.associatedMessagePart ?? 0) == e.part),
+                                          message: message,
+                                        ),
+                                      ),
+                                    if (!message.isFromMe!)
+                                      Positioned(
+                                        top: -15,
+                                        right: -20,
+                                        child: ReactionHolder(
+                                          reactions: reactions.where((s) => (s.associatedMessagePart ?? 0) == e.part),
+                                          message: message,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              // swipe to reply
+                              if (canSwipeToReply && !message.isGroupEvent && !e.isUnsent)
+                                Obx(() => SlideToReply(width: replyOffsets[index].value.abs())),
+                            ].conditionalReverse(message.isFromMe!),
+                          ),
+                          // message properties (replies, edits, effect)
+                          MessageProperties(parentController: controller, part: e),
+                        ],
+                      )),
+                      // delivered / read receipt
+                      if (message.isFromMe!)
+                        DeliveredIndicator(parentController: controller),
                     ],
-                  )),
-                  if (message.isFromMe!)
-                    DeliveredIndicator(parentController: controller),
-                ],
-              ),
+                  ),
+                ),
+                // slide to view timestamp
+                MessageTimestamp(controller: controller, cvController: widget.cvController),
+              ],
             ),
-            MessageTimestamp(controller: controller, cvController: widget.cvController),
           ],
         ),
       ],
