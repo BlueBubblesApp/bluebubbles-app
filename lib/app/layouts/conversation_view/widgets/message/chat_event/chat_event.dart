3ff4fcdcec8f13c69ea82012657468c07d0b1bc9
Tanay Neotia <tanay@neotia.in>
2022-12-01 19:03:11 -0500

    Show message info when long pressing chat event text

--- lib/app/layouts/conversation_view/widgets/message/chat_event/chat_event.dart
+++ lib/app/layouts/conversation_view/widgets/message/chat_event/chat_event.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:bluebubbles/models/models.dart';
+import 'package:bluebubbles/helpers/helpers.dart';
+import 'package:bluebubbles/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -17,14 +21,56 @@ class ChatEvent extends StatelessWidget {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        child: Text(
-          part.isUnsent
-              ? (message.isFromMe! ? "You unsent a message. Others may still see the message on devices where the software hasn't been updated" : "${message.handle?.displayName ?? "Unknown"} unsent a message")
-              : message.groupEventText,
-          style: context.theme.textTheme.bodySmall!.copyWith(color: context.theme.colorScheme.outline),
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-          textAlign: TextAlign.center,
+        child: GestureDetector(
+          onLongPress: () {
+            const encoder = JsonEncoder.withIndent("     ");
+            final str = encoder.convert(message.toMap(includeObjects: true));
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text(
+                  "Message Info",
+                  style: context.theme.textTheme.titleLarge,
+                ),
+                backgroundColor: context.theme.colorScheme.properSurface,
+                content: SizedBox(
+                  width: ns.width(context) * 3 / 5,
+                  height: context.height * 1 / 4,
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: context.theme.backgroundColor,
+                        borderRadius: const BorderRadius.all(Radius.circular(10))
+                    ),
+                    child: SingleChildScrollView(
+                      child: SelectableText(
+                        str,
+                        style: context.theme.textTheme.bodyLarge,
+                      ),
+                    ),
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    child: Text(
+                        "Close",
+                        style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary)
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            );
+          },
+          child: Text(
+            part.isUnsent
+                ? (message.isFromMe! ? "You unsent a message. Others may still see the message on devices where the software hasn't been updated" : "${message.handle?.displayName ?? "Unknown"} unsent a message")
+                : message.groupEventText,
+            style: context.theme.textTheme.bodySmall!.copyWith(color: context.theme.colorScheme.outline),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
