d495da23ac8bc57818ef23b506d8fd1fd7c8d104
Tanay Neotia <tanay@neotia.in>
2022-12-06 12:58:33 -0500

    Desktop won't use background isolate ever

--- lib/services/backend/java_dart_interop/background_isolate.dart
+++ lib/services/backend/java_dart_interop/background_isolate.dart
@@ -25,40 +25,18 @@ backgroundIsolateEntrypoint() async {
   await fs.init(headless: true);
   await mcs.init(headless: true);
   Directory objectBoxDirectory = Directory(join(fs.appDocDir.path, 'objectbox'));
-  if (!kIsDesktop) {
-    debugPrint("Trying to attach to an existing ObjectBox store");
+  debugPrint("Trying to attach to an existing ObjectBox store");
+  try {
+    store = Store.attach(getObjectBoxModel(), objectBoxDirectory.path);
+  } catch (e, s) {
+    debugPrint(e.toString());
+    debugPrint(s.toString());
+    debugPrint("Failed to attach to existing store, opening from path");
     try {
-      store = Store.attach(getObjectBoxModel(), objectBoxDirectory.path);
-    } catch (e, s) {
-      debugPrint(e.toString());
-      debugPrint(s.toString());
-      debugPrint("Failed to attach to existing store, opening from path");
-      try {
-        store = await openStore(directory: objectBoxDirectory.path);
-      } catch (e, s) {
-        debugPrint(e.toString());
-        debugPrint(s.toString());
-      }
-    }
-  } else {
-    try {
-      await objectBoxDirectory.create(recursive: true);
-      debugPrint("Opening ObjectBox store from path: ${objectBoxDirectory.path}");
       store = await openStore(directory: objectBoxDirectory.path);
     } catch (e, s) {
       debugPrint(e.toString());
       debugPrint(s.toString());
-      if (Platform.isWindows) {
-        debugPrint("Failed to open store from default path. Using custom path");
-        const customStorePath = "C:\\bluebubbles_app";
-        ss.prefs.setBool("use-custom-path", true);
-        ss.prefs.setString("custom-path", customStorePath);
-        objectBoxDirectory = Directory(join(customStorePath, "objectbox"));
-        await objectBoxDirectory.create(recursive: true);
-        debugPrint("Opening ObjectBox store from custom path: ${objectBoxDirectory.path}");
-        store = await openStore(directory: join(customStorePath, 'objectbox'));
-      }
-      // TODO Linux fallback
     }
   }
   debugPrint("Opening boxes");
