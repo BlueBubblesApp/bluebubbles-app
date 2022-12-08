c3f54e17249509b8547d2ac2ddf97f625067c85c
Tanay Neotia <tanay@neotia.in>
2022-12-08 12:58:43 -0500

    No need to make this final

--- lib/services/ui/chat/conversation_view_controller.dart
+++ lib/services/ui/chat/conversation_view_controller.dart
@@ -24,7 +24,7 @@ ConversationViewController cvc(Chat chat, {String? tag}) => Get.isRegistered<Con
 class ConversationViewController extends StatefulController with SingleGetTickerProviderMixin {
   final Chat chat;
   late final String tag;
-  late final bool fromChatCreator;
+  bool fromChatCreator = false;
   final AutoScrollController scrollController = AutoScrollController();
 
   ConversationViewController(this.chat, {String? tag_}) {
