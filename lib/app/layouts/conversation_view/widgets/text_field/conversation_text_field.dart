6fdaac2e9452c707e05ee2754d355b72a7ecb6a1
Tanay Neotia <tanay@neotia.in>
2022-12-25 09:37:22 -0500

    Re-add content commit again

--- lib/app/layouts/conversation_view/widgets/text_field/conversation_text_field.dart
+++ lib/app/layouts/conversation_view/widgets/text_field/conversation_text_field.dart
@@ -675,7 +675,7 @@ class TextFieldComponent extends StatelessWidget {
     );
   }
 
-  void onContentCommit(dynamic content) async {
+  void onContentCommit(CommittedContent content) async {
     // Add some debugging logs
     Logger.info("[Content Commit] Keyboard received content");
     Logger.info("  -> Content Type: ${content.mimeType}");
