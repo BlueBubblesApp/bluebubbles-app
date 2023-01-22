4a6a7143b295f9df247f99ac0f10594109a5af96
Joel Jothiprakasam <hijoelj@gmail.com>
2023-01-22 14:03:27 -0600

    Remove unnecessary code
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/app/layouts/setup/pages/sync/server_credentials.dart
+++ lib/app/layouts/setup/pages/sync/server_credentials.dart
@@ -137,167 +137,158 @@ class _ServerCredentialsState extends OptimizedState<ServerCredentials> {
                         labelStyle: TextStyle(color: context.theme.colorScheme.outline),
                       ),
                     ),
-                    child: FocusTraversalGroup(
-                      policy: OrderedTraversalPolicy(),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          Container(
-                            width: context.width * 2 / 3,
-                            child: FocusTraversalOrder(
-                              order: const NumericFocusOrder(1.0),
-                              child: Focus(
-                                onKey: (node, event) {
-                                  if (event is RawKeyDownEvent && !event.data.isShiftPressed && event.logicalKey == LogicalKeyboardKey.tab) {
-                                    node.nextFocus();
-                                    return KeyEventResult.handled;
-                                  }
-                                  return KeyEventResult.ignored;
-                                },
-                                child: TextField(
-                                  cursorColor: context.theme.colorScheme.primary,
-                                  autocorrect: false,
-                                  autofocus: true,
-                                  controller: urlController,
-                                  textInputAction: TextInputAction.next,
-                                  decoration: InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: context.theme.colorScheme.outline), borderRadius: BorderRadius.circular(20)),
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: context.theme.colorScheme.primary), borderRadius: BorderRadius.circular(20)),
-                                    labelText: "URL",
-                                  ),
-                                ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        Container(
+                          width: context.width * 2 / 3,
+                          child: Focus(
+                            onKey: (node, event) {
+                              if (event is RawKeyDownEvent && !event.data.isShiftPressed && event.logicalKey == LogicalKeyboardKey.tab) {
+                                node.nextFocus();
+                                return KeyEventResult.handled;
+                              }
+                              return KeyEventResult.ignored;
+                            },
+                            child: TextField(
+                              cursorColor: context.theme.colorScheme.primary,
+                              autocorrect: false,
+                              autofocus: true,
+                              controller: urlController,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: context.theme.colorScheme.outline), borderRadius: BorderRadius.circular(20)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: context.theme.colorScheme.primary), borderRadius: BorderRadius.circular(20)),
+                                labelText: "URL",
                               ),
                             ),
                           ),
-                          const SizedBox(height: 20),
-                          Container(
-                            width: context.width * 2 / 3,
-                            child: FocusTraversalOrder(
-                              order: const NumericFocusOrder(2.0),
-                              child: Focus(
-                                onKey: (node, event) {
-                                  if (event is RawKeyDownEvent && event.data.isShiftPressed && event.logicalKey == LogicalKeyboardKey.tab) {
-                                    node.previousFocus();
-                                    node.previousFocus(); // This is intentional. Should probably figure out why it's needed
-                                    return KeyEventResult.handled;
-                                  }
-                                  return KeyEventResult.ignored;
-                                },
-                                child: TextField(
-                                  cursorColor: context.theme.colorScheme.primary,
-                                  autocorrect: false,
-                                  autofocus: false,
-                                  controller: passwordController,
-                                  textInputAction: TextInputAction.next,
-                                  onSubmitted: (pass) => connect(urlController.text, pass),
-                                  decoration: InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: context.theme.colorScheme.outline), borderRadius: BorderRadius.circular(20)),
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: context.theme.colorScheme.primary), borderRadius: BorderRadius.circular(20)),
-                                    labelText: "Password",
-                                    contentPadding: const EdgeInsets.fromLTRB(12, 24, 40, 16),
-                                    suffixIcon: IconButton(
-                                      icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
-                                      color: context.theme.colorScheme.outline,
-                                      onPressed: () {
-                                        setState(() {
-                                          obscureText = !obscureText;
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                  obscureText: obscureText,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(25),
-                                  gradient: LinearGradient(
-                                    begin: AlignmentDirectional.topStart,
-                                    colors: [HexColor('2772C3'), HexColor('5CA7F8').darkenPercent(5)],
-                                  ),
-                                ),
-                                height: 40,
-                                padding: const EdgeInsets.all(2),
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20.0),
-                                      ),
-                                    ),
-                                    backgroundColor: MaterialStateProperty.all(context.theme.colorScheme.background),
-                                    shadowColor: MaterialStateProperty.all(context.theme.colorScheme.background),
-                                    maximumSize: MaterialStateProperty.all(const Size(200, 36)),
-                                    minimumSize: MaterialStateProperty.all(const Size(30, 30)),
-                                  ),
-                                  onPressed: () async {
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: context.width * 2 / 3,
+                          child: Focus(
+                            onKey: (node, event) {
+                              if (event is RawKeyDownEvent && event.data.isShiftPressed && event.logicalKey == LogicalKeyboardKey.tab) {
+                                node.previousFocus();
+                                node.previousFocus(); // This is intentional. Should probably figure out why it's needed
+                                return KeyEventResult.handled;
+                              }
+                              return KeyEventResult.ignored;
+                            },
+                            child: TextField(
+                              cursorColor: context.theme.colorScheme.primary,
+                              autocorrect: false,
+                              autofocus: false,
+                              controller: passwordController,
+                              textInputAction: TextInputAction.next,
+                              onSubmitted: (pass) => connect(urlController.text, pass),
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: context.theme.colorScheme.outline), borderRadius: BorderRadius.circular(20)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: context.theme.colorScheme.primary), borderRadius: BorderRadius.circular(20)),
+                                labelText: "Password",
+                                contentPadding: const EdgeInsets.fromLTRB(12, 24, 40, 16),
+                                suffixIcon: IconButton(
+                                  icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
+                                  color: context.theme.colorScheme.outline,
+                                  onPressed: () {
                                     setState(() {
-                                      showManualEntry = false;
+                                      obscureText = !obscureText;
                                     });
                                   },
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(Icons.close, color: context.theme.colorScheme.onBackground, size: 20),
-                                      const SizedBox(width: 10),
-                                      Text("Cancel",
-                                          style: context.theme.textTheme.bodyLarge!
-                                              .apply(fontSizeFactor: 1.1, color: context.theme.colorScheme.onBackground)),
-                                    ],
-                                  ),
                                 ),
                               ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(25),
-                                  gradient: LinearGradient(
-                                    begin: AlignmentDirectional.topStart,
-                                    colors: [HexColor('2772C3'), HexColor('5CA7F8').darkenPercent(5)],
-                                  ),
-                                ),
-                                height: 40,
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20.0),
-                                      ),
-                                    ),
-                                    backgroundColor: MaterialStateProperty.all(Colors.transparent),
-                                    shadowColor: MaterialStateProperty.all(Colors.transparent),
-                                    maximumSize: MaterialStateProperty.all(const Size(200, 36)),
-                                    minimumSize: MaterialStateProperty.all(const Size(30, 30)),
-                                  ),
-                                  onPressed: () async {
-                                    connect(urlController.text, passwordController.text);
-                                  },
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text("Connect", style: context.theme.textTheme.bodyLarge!.apply(fontSizeFactor: 1.1, color: Colors.white)),
-                                      const SizedBox(width: 10),
-                                      const Icon(Icons.arrow_forward, color: Colors.white, size: 20),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
+                              obscureText: obscureText,
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                gradient: LinearGradient(
+                                  begin: AlignmentDirectional.topStart,
+                                  colors: [HexColor('2772C3'), HexColor('5CA7F8').darkenPercent(5)],
+                                ),
+                              ),
+                              height: 40,
+                              padding: const EdgeInsets.all(2),
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                  ),
+                                  backgroundColor: MaterialStateProperty.all(context.theme.colorScheme.background),
+                                  shadowColor: MaterialStateProperty.all(context.theme.colorScheme.background),
+                                  maximumSize: MaterialStateProperty.all(const Size(200, 36)),
+                                  minimumSize: MaterialStateProperty.all(const Size(30, 30)),
+                                ),
+                                onPressed: () async {
+                                  setState(() {
+                                    showManualEntry = false;
+                                  });
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(Icons.close, color: context.theme.colorScheme.onBackground, size: 20),
+                                    const SizedBox(width: 10),
+                                    Text("Cancel",
+                                        style: context.theme.textTheme.bodyLarge!
+                                            .apply(fontSizeFactor: 1.1, color: context.theme.colorScheme.onBackground)),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                gradient: LinearGradient(
+                                  begin: AlignmentDirectional.topStart,
+                                  colors: [HexColor('2772C3'), HexColor('5CA7F8').darkenPercent(5)],
+                                ),
+                              ),
+                              height: 40,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                  ),
+                                  backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                                  shadowColor: MaterialStateProperty.all(Colors.transparent),
+                                  maximumSize: MaterialStateProperty.all(const Size(200, 36)),
+                                  minimumSize: MaterialStateProperty.all(const Size(30, 30)),
+                                ),
+                                onPressed: () async {
+                                  connect(urlController.text, passwordController.text);
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text("Connect", style: context.theme.textTheme.bodyLarge!.apply(fontSizeFactor: 1.1, color: Colors.white)),
+                                    const SizedBox(width: 10),
+                                    const Icon(Icons.arrow_forward, color: Colors.white, size: 20),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-          )
+          ),
         ],
       ),
     );
