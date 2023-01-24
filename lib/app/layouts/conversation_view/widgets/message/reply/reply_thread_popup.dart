52374136b066a84a5f431f976d72a0fa7b0ab02e
Joel Jothiprakasam <hijoelj@gmail.com>
2023-01-24 12:23:12 -0600

    Make chat overlays look good on desktop when window effects enabled by disabling blur on non-fullscreen popups
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/app/layouts/conversation_view/widgets/message/reply/reply_thread_popup.dart
+++ lib/app/layouts/conversation_view/widgets/message/reply/reply_thread_popup.dart
@@ -29,70 +29,87 @@ void showReplyThread(BuildContext context, Message message, MessagePart part, Me
       pageBuilder: (context, animation, secondaryAnimation) {
         // Future.delayed(Duration.zero, () => controller.jumpTo(controller.position.maxScrollExtent));
         return FadeTransition(
-          opacity: animation,
-          child: Theme(
-            data: context.theme.copyWith(
-              // in case some components still use legacy theming
-              primaryColor: context.theme.colorScheme.bubble(context, true),
-              colorScheme: context.theme.colorScheme.copyWith(
-                primary: context.theme.colorScheme.bubble(context, true),
-                onPrimary: context.theme.colorScheme.onBubble(context, true),
-                surface: ss.settings.monetTheming.value == Monet.full ? null : (context.theme.extensions[BubbleColors] as BubbleColors?)?.receivedBubbleColor,
-                onSurface: ss.settings.monetTheming.value == Monet.full ? null : (context.theme.extensions[BubbleColors] as BubbleColors?)?.onReceivedBubbleColor,
+            opacity: animation,
+            child: Theme(
+              data: context.theme.copyWith(
+                // in case some components still use legacy theming
+                primaryColor: context.theme.colorScheme.bubble(context, true),
+                colorScheme: context.theme.colorScheme.copyWith(
+                  primary: context.theme.colorScheme.bubble(context, true),
+                  onPrimary: context.theme.colorScheme.onBubble(context, true),
+                  surface: ss.settings.monetTheming.value == Monet.full
+                      ? null
+                      : (context.theme.extensions[BubbleColors] as BubbleColors?)?.receivedBubbleColor,
+                  onSurface: ss.settings.monetTheming.value == Monet.full
+                      ? null
+                      : (context.theme.extensions[BubbleColors] as BubbleColors?)?.onReceivedBubbleColor,
+                ),
               ),
-            ),
-            child: DeferredPointerHandler(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                child: AnnotatedRegion<SystemUiOverlayStyle>(
-                  value: SystemUiOverlayStyle(
-                    systemNavigationBarColor: ss.settings.immersiveMode.value ? Colors.transparent : context.theme.colorScheme.background, // navigation bar color
-                    systemNavigationBarIconBrightness: context.theme.colorScheme.brightness,
-                    statusBarColor: Colors.transparent, // status bar color
-                    statusBarIconBrightness: context.theme.colorScheme.brightness.opposite,
-                  ),
-                  child: Scaffold(
-                    backgroundColor: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled ? context.theme.colorScheme.properSurface.withOpacity(0.6) : Colors.transparent,
-                    body: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled ? 10 : 30, sigmaY: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled ? 10 : 30),
-                      child: Container(
-                        color: context.theme.colorScheme.background.withAlpha(150),
-                        child: SafeArea(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Center(
-                              child: SingleChildScrollView(
-                                controller: controller,
-                                child: Column(
-                                  children: _messages.mapIndexed((index, e) => AbsorbPointer(
-                                    absorbing: true,
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(left: 5.0, right: 5.0),
-                                      child: MessageHolder(
-                                        cvController: cvc(service.chat, tag: service.chat.guid),
-                                        message: _messages[index],
-                                        oldMessageGuid: index > 0 ? _messages[index - 1].guid : null,
-                                        newMessageGuid: index < _messages.length - 1 ? _messages[index + 1].guid : null,
-                                        isReplyThread: true,
-                                        replyPart: index == 0 ? originatorPart : null,
-                                      ),
-                                    ),
-                                  )).toList(),
-                                )
+              child: DeferredPointerHandler(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: AnnotatedRegion<SystemUiOverlayStyle>(
+                    value: SystemUiOverlayStyle(
+                      systemNavigationBarColor: ss.settings.immersiveMode.value ? Colors.transparent : context.theme.colorScheme.background,
+                      // navigation bar color
+                      systemNavigationBarIconBrightness: context.theme.colorScheme.brightness,
+                      statusBarColor: Colors.transparent,
+                      // status bar color
+                      statusBarIconBrightness: context.theme.colorScheme.brightness.opposite,
+                    ),
+                    child: Scaffold(
+                      backgroundColor: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled
+                          ? context.theme.colorScheme.properSurface.withOpacity(0.9)
+                          : Colors.transparent,
+                      body: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          BackdropFilter(
+                            filter: ImageFilter.blur(
+                                sigmaX: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled ? 0 : 30,
+                                sigmaY: kIsDesktop && ss.settings.windowEffect.value != WindowEffect.disabled ? 0 : 30),
+                            child: Container(
+                              color: context.theme.colorScheme.properSurface.withOpacity(0.3),
+                            ),
+                          ),
+                          Container(
+                            child: SafeArea(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Center(
+                                  child: SingleChildScrollView(
+                                      controller: controller,
+                                      child: Column(
+                                        children: _messages
+                                            .mapIndexed((index, e) => AbsorbPointer(
+                                                  absorbing: true,
+                                                  child: Padding(
+                                                    padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+                                                    child: MessageHolder(
+                                                      cvController: cvc(service.chat, tag: service.chat.guid),
+                                                      message: _messages[index],
+                                                      oldMessageGuid: index > 0 ? _messages[index - 1].guid : null,
+                                                      newMessageGuid: index < _messages.length - 1 ? _messages[index + 1].guid : null,
+                                                      isReplyThread: true,
+                                                      replyPart: index == 0 ? originatorPart : null,
+                                                    ),
+                                                  ),
+                                                ))
+                                            .toList(),
+                                      )),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          )
-        );
+            ));
       },
       fullscreenDialog: true,
       opaque: false,
