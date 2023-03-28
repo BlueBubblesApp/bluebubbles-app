35f21040209f73c8ffaac013f55f61aa780448b0
Tanay Neotia <tanay@neotia.in>
2023-03-28 09:30:24 -0400

    Disable editing for group photo

--- lib/app/layouts/conversation_details/widgets/chat_info.dart
+++ lib/app/layouts/conversation_details/widgets/chat_info.dart
@@ -168,7 +168,7 @@ class _ChatInfoState extends OptimizedState<ChatInfo> {
                     child: ContactAvatarGroupWidget(
                       chat: chat,
                       size: 100,
-                      editable: false,
+                      editable: !chat.isGroup,
                     ),
                   ),
                   Obx(() => chat.customAvatarPath != null ? Positioned(
