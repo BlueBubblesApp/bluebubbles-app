cfdf0f92daa38eead6bd9f6211dc5d7c90297b83
Joel Jothiprakasam <hijoelj@gmail.com>
2023-04-22 18:29:46 -0500

    Don't call unsupported functions linux
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/utils/share.dart
+++ lib/utils/share.dart
@@ -9,6 +9,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
 import 'package:share_plus/share_plus.dart' as sp;
+import 'package:universal_io/io.dart';
 
 class Share {
   /// Share a file with other apps.
@@ -33,47 +34,56 @@ class Share {
     _serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!_serviceEnabled) {
       await showDialog(
-        context: Get.context!,
-        builder: (context) => AlertDialog(
-          backgroundColor: Get.theme.colorScheme.properSurface,
-          title: Text("Location Services", style: Get.textTheme.titleLarge,),
-          content: Text(
-            "Location Services must be enabled to send Locations",
-            style: Get.textTheme.bodyLarge,
-          ),
-          actions: [
-            TextButton(onPressed: () => Get.back(), child: Text("Cancel", style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary))),
-            TextButton(onPressed: () async => await Geolocator.openLocationSettings(), child: Text("Open Settings", style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary)))
-          ],
-        )
+          context: Get.context!,
+          builder: (context) => AlertDialog(
+            backgroundColor: Get.theme.colorScheme.properSurface,
+            title: Text("Location Services", style: Get.textTheme.titleLarge,),
+            content: Text(
+              "Location Services must be enabled to send Locations",
+              style: Get.textTheme.bodyLarge,
+            ),
+            actions: [
+              if (!kIsDesktop || !Platform.isLinux)
+                TextButton(onPressed: () => Get.back(), child: Text("Cancel", style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary))),
+              if (!kIsDesktop || !Platform.isLinux)
+                TextButton(onPressed: () async => await Geolocator.openLocationSettings(), child: Text("Open Settings", style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary))),
+              if (kIsDesktop && Platform.isLinux)
+                TextButton(onPressed: () => Get.back(), child: Text("OK", style: context.theme.textTheme.bodyLarge!.copyWith(color: context.theme.colorScheme.primary))),
+            ],
+          )
       );
       if (!_serviceEnabled) {
         return;
       }
     }
 
-    _permissionGranted = await Geolocator.checkPermission();
-    if (_permissionGranted == LocationPermission.denied) {
-      _permissionGranted = await Geolocator.requestPermission();
-    }
-    if (_permissionGranted == LocationPermission.denied || _permissionGranted == LocationPermission.deniedForever) {
-      await showDialog(
-        context: Get.context!,
-        builder: (context) => AlertDialog(
-          backgroundColor: Get.theme.colorScheme.properSurface,
-          title: Text("Location Permission", style: Get.textTheme.titleLarge),
-          content: Text(
-            "BlueBubbles needs the Location permission to send Locations",
-            style: Get.textTheme.bodyLarge,
-          ),
-          actions: [
-            TextButton(onPressed: () => Get.back(), child: Text("Cancel", style: Get.textTheme.bodyLarge!.copyWith(color: Get.theme.colorScheme.primary))),
-            TextButton(onPressed: () async => await Geolocator.openLocationSettings(), child: Text("Open Settings", style: Get.textTheme.bodyLarge!.copyWith(color: Get.theme.colorScheme.primary)))
-          ],
-        )
-      );
-      if (_permissionGranted != LocationPermission.whileInUse && _permissionGranted != LocationPermission.always) {
-        return;
+    if (!kIsDesktop || !Platform.isLinux) {
+      _permissionGranted = await Geolocator.checkPermission();
+      if (_permissionGranted == LocationPermission.denied) {
+        _permissionGranted = await Geolocator.requestPermission();
+      }
+      if (_permissionGranted == LocationPermission.denied || _permissionGranted == LocationPermission.deniedForever) {
+        await showDialog(
+            context: Get.context!,
+            builder: (context) =>
+                AlertDialog(
+                  backgroundColor: Get.theme.colorScheme.properSurface,
+                  title: Text("Location Permission", style: Get.textTheme.titleLarge),
+                  content: Text(
+                    "BlueBubbles needs the Location permission to send Locations",
+                    style: Get.textTheme.bodyLarge,
+                  ),
+                  actions: [
+                    TextButton(onPressed: () => Get.back(),
+                        child: Text("Cancel", style: Get.textTheme.bodyLarge!.copyWith(color: Get.theme.colorScheme.primary))),
+                    TextButton(onPressed: () async => await Geolocator.openLocationSettings(),
+                        child: Text("Open Settings", style: Get.textTheme.bodyLarge!.copyWith(color: Get.theme.colorScheme.primary)))
+                  ],
+                )
+        );
+        if (_permissionGranted == LocationPermission.denied || _permissionGranted == LocationPermission.deniedForever) {
+          return;
+        }
       }
     }
 
