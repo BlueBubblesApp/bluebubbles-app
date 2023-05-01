a4e969587f3dad737a38572996edb971515e09ac
Tanay Neotia <tanay@neotia.in>
2023-05-01 16:41:25 -0400

    Fix log out on web

--- lib/app/layouts/conversation_list/widgets/header/header_widgets.dart
+++ lib/app/layouts/conversation_list/widgets/header/header_widgets.dart
@@ -145,8 +145,6 @@ class OverflowMenu extends StatelessWidget {
                       await ss.prefs.clear();
                       await ss.prefs.setString("selected-dark", "OLED Dark");
                       await ss.prefs.setString("selected-light", "Bright White");
-                      themeBox.putMany(ts.defaultThemes);
-                      await ts.changeTheme(context);
                       Get.offAll(() => WillPopScope(
                         onWillPop: () async => false,
                         child: TitleBarWrapper(child: SetupView()),
