2799c469f691cf3cead8714d972d9184237cc2f8
Tanay Neotia <tanay@neotia.in>
2023-05-02 11:09:27 -0400

    Java implementation of Cloud Firestore

--- android/app/src/main/java/com/bluebubbles/messaging/firebase/DBListener.java
+++ android/app/src/main/java/com/bluebubbles/messaging/firebase/DBListener.java
@@ -1,12 +1,16 @@
 package com.bluebubbles.messaging.firebase;
 
 import android.util.Log;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import io.flutter.plugin.common.MethodChannel;
 
@@ -14,7 +18,7 @@ import static com.bluebubbles.messaging.MainActivity.CHANNEL;
 import static com.bluebubbles.messaging.MainActivity.engine;
 
 public class DBListener {
-    public static ValueEventListener dbListener = new ValueEventListener() {
+    public static ValueEventListener rtdbListener = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
             Log.d("FirebaseDB", "Firebase Database updated. Syncing...");
@@ -28,6 +32,23 @@ public class DBListener {
         @Override
         public void onCancelled(@NonNull DatabaseError databaseError) {
 
+        }
+    };
+
+    public static EventListener<DocumentSnapshot> cfdbListener = new EventListener<DocumentSnapshot>() {
+        @Override
+        public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException error) {
+            if (documentSnapshot != null) {
+                Log.d("FirebaseDB", "Realtime database updated. Syncing...");
+                Map data = documentSnapshot.getData();
+                if (data != null) {
+                    String serverURL = data.get("serverUrl").toString();
+                    Log.d("FirebaseDB", "Current server URL: " + serverURL);
+                    if (engine != null) {
+                        new MethodChannel(engine.getDartExecutor().getBinaryMessenger(), CHANNEL).invokeMethod("new-server", "[" + serverURL + "]");
+                    }
+                }
+            }
         }
     };
 }
