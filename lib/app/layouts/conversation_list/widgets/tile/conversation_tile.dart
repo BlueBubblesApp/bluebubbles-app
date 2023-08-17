9e0c67c87bf4e08676dfd7025fbb785fd955dddd
Joel Jothiprakasam <hijoelj@gmail.com>
2023-08-17 09:19:02 -0700

    Fix possible bug
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

--- lib/app/layouts/conversation_list/widgets/tile/conversation_tile.dart
+++ lib/app/layouts/conversation_list/widgets/tile/conversation_tile.dart
@@ -138,7 +138,7 @@ class _ConversationTileState extends CustomState<ConversationTile, void, Convers
       if (event.item1 == 'update-highlight' && mounted) {
         if ((kIsDesktop || kIsWeb) && event.item2 == controller.chat.guid) {
           controller.shouldHighlight.value = true;
-        } else if (controller.shouldHighlight.value = true) {
+        } else if (controller.shouldHighlight.value) {
           controller.shouldHighlight.value = false;
         }
       }
