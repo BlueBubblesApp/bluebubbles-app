88b6ec1795318cab248fc15de8a9b22c353ea9ca
Joel Jothiprakasam <hijoelj@gmail.com>
2023-08-26 14:45:13 -0500

    Add build and tar script for linux
    Signed-off-by: Joel Jothiprakasam <hijoelj@gmail.com>

---
+++ linux/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+
+cd "$(dirname "$0")/.."
+flutter build linux --release -v
+
+arch=$(uname -m)
+if [[ $arch == "x86_64" ]]; then
+    folder="x64"
+elif [[ $arch == "aarch64" ]]; then
+    folder="arm64"
+fi
+
+cd ..
+tar cvf bluebubbles-linux-"$arch".tar -C build/linux/$folder/release/bundle .
