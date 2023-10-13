abf20a81cad440761ecd930fa157742b4b82e2b5
Joel Jothiprakasam <hijoelj@gmail.com>
2023-10-13 03:39:43 -0500

    Revert "Add window effect laggy disclaimer (#2567)"
    This reverts commit 50132d6f1067343db30af1f07d90b3d1ea79ad9f.

--- lib/app/layouts/settings/pages/theming/theming_panel.dart
+++ lib/app/layouts/settings/pages/theming/theming_panel.dart
@@ -245,7 +245,7 @@ class _ThemingPanelState extends CustomState<ThemingPanel, void, ThemingPanelCon
                             saveSettings();
                           },
                           title: "Window Effect",
-                          subtitle: "${WindowEffects.descriptions[ss.settings.windowEffect.value]}\n\nOperating System Version: ${Platform.operatingSystemVersion}\nBuild number: ${parsedWindowsVersion()}\nNote: Certain effects may cause window movements to become choppy",
+                          subtitle: "${WindowEffects.descriptions[ss.settings.windowEffect.value]}\n\nOperating System Version: ${Platform.operatingSystemVersion}\nBuild number: ${parsedWindowsVersion()}",
                           backgroundColor: tileColor,
                           secondaryColor: headerColor,
                           capitalize: true,
@@ -255,7 +255,7 @@ class _ThemingPanelState extends CustomState<ThemingPanel, void, ThemingPanelCon
                         Obx(() => SettingsSubtitle(
                               unlimitedSpace: true,
                               subtitle:
-                                  "${WindowEffects.descriptions[ss.settings.windowEffect.value]}\n\nOperating System Version: ${Platform.operatingSystemVersion}\nBuild number: ${parsedWindowsVersion()}\nNote: Certain effects may cause window movements to become choppy",
+                                  "${WindowEffects.descriptions[ss.settings.windowEffect.value]}\n\nOperating System Version: ${Platform.operatingSystemVersion}\nBuild number: ${parsedWindowsVersion()}",
                             )),
                       Obx(() {
                         if (WindowEffects.dependsOnColor() && !WindowEffects.isDark(color: context.theme.colorScheme.background)) {
