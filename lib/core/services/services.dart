41fdbff6ec6414601c1a6cde56ba5dd00f911978
zlshames <zlshames@gmail.com>
2023-11-01 21:30:50 -0400

    adds network service to master services

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -1,3 +1,4 @@
+import 'package:bluebubbles/core/abstractions/network/network_service.dart';
 import 'package:bluebubbles/core/abstractions/storage/database_service.dart';
 import 'package:bluebubbles/core/abstractions/device_service.dart';
 import 'package:bluebubbles/core/abstractions/server_service.dart';
@@ -8,10 +9,12 @@ import 'package:bluebubbles/core/services/github_update_service.dart';
 import 'package:bluebubbles/core/services/default/default_device_service.dart';
 import 'package:bluebubbles/core/services/default/default_server_service.dart';
 import 'package:bluebubbles/core/services/default/default_settings_service.dart';
+import 'package:bluebubbles/core/services/network/dio/dio_network_service.dart';
 import 'package:bluebubbles/core/services/objectbox_database_service.dart';
 import 'package:bluebubbles/core/services/default/default_shared_prefs_service.dart';
 import 'package:get/get.dart';
 
+NetworkService network = Get.isRegistered<DioNetworkService>() ? Get.find<DioNetworkService>() : Get.put(DioNetworkService());
 DatabaseService db = Get.isRegistered<ObjectBoxDatabaseService>() ? Get.find<ObjectBoxDatabaseService>() : Get.put(ObjectBoxDatabaseService());
 SettingsService settings = Get.isRegistered<DefaultSettingsService>() ? Get.find<DefaultSettingsService>() : Get.put(DefaultSettingsService());
 SharedPreferenceService prefs = Get.isRegistered<DefaultSharedPrefsService>() ? Get.find<DefaultSharedPrefsService>() : Get.put(DefaultSharedPrefsService());
