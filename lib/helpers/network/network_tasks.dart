5072bc8193909a01f1579c498d7d2a772bfbaa33
zlshames <zlshames@gmail.com>
2024-04-18 11:06:57 -0400

    fix: [#2691

--- lib/helpers/network/network_tasks.dart
+++ lib/helpers/network/network_tasks.dart
@@ -117,6 +117,13 @@ class NetworkTasks {
 
     if (http.originOverride != null) return;
 
+    // This was moved from main.dart to here because this is the only place we use it.
+    // This will also make an API call to a github file containing a mapping of MAC addresses
+    // to vendor information. That info is used to display metadata about an ActiveHost found
+    // on the network via a port scan. We don't want that API call to happen on first-boot, nor
+    // do we need it to.
+    await configureNetworkTools(fs.appDocDir.path, enableDebugging: kDebugMode);
+
     Logger.debug("Falling back to port scanning");
     final wifiIP = await NetworkInfo().getWifiIP();
     if (wifiIP != null) {
