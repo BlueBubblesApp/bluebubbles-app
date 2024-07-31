c48e8b9e44de4d1e0e3e664b18bb51c3a8f26730
Tae Hagen <tae.hagen@gmail.com>
2024-07-31 11:42:09 -0600

    feat: CopperBoy's better cupertino header

--- lib/app/layouts/conversation_view/widgets/header/cupertino_header.dart
+++ lib/app/layouts/conversation_view/widgets/header/cupertino_header.dart
@@ -23,6 +23,21 @@ class CupertinoHeader extends StatelessWidget implements PreferredSizeWidget {
 
   final ConversationViewController controller;
 
+  // simulate apple's saturatioon
+  static const List<double> darkMatrix = <double>[
+    1.385, -0.56, -0.112, 0.0, 0.3, //
+    -0.315, 1.14, -0.112, 0.0, 0.3, //
+    -0.315, -0.56, 1.588, 0.0, 0.3, //
+    0.0, 0.0, 0.0, 1.0, 0.0
+  ];
+
+  static const List<double> lightMatrix = <double>[
+    1.74, -0.4, -0.17, 0.0, 0.0, //
+    -0.26, 1.6, -0.17, 0.0, 0.0, //
+    -0.26, -0.4, 1.83, 0.0, 0.0, //
+    0.0, 0.0, 0.0, 1.0, 0.0
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ClipRect(
