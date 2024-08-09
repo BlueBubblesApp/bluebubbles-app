a8ce2bd72c6939df644c8baf4d43d476604546f1
zlshames <zlshames@gmail.com>
2024-08-08 21:04:45 -0400

    improved logger code

--- lib/utils/logger/logger.dart
+++ lib/utils/logger/logger.dart
@@ -1,11 +1,14 @@
+import 'package:archive/archive_io.dart';
 import 'package:bluebubbles/services/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart';
-
-import 'outputs/debug_console_output.dart';
+import 'package:universal_io/io.dart';
 
 // ignore: library_prefixes
 import 'package:logger/logger.dart' as LoggerFactory;
+
+import 'outputs/debug_console_output.dart';
 
 // ignore: non_constant_identifier_names
 BaseLogger Logger = Get.isRegistered<BaseLogger>() ? Get.find<BaseLogger>() : Get.put(BaseLogger());
@@ -14,13 +17,12 @@ enum LogLevel { INFO, WARN, ERROR, DEBUG, TRACE, FATAL }
 
 class BaseLogger extends GetxService {
   late final LoggerFactory.Logger logger;
-  final logName = "bluebubbles.log";
 
   String get logDir {
     return join(fs.appDocDir.path, 'logs');
   }
 
-  Future<void> init({bool isStartup = false}) async {
+  Future<void> init() async {
     logger = createLogger(filter: LoggerFactory.ProductionFilter());
   }
 
@@ -31,7 +33,7 @@ class BaseLogger extends GetxService {
         methodCount: 0, // Number of method calls to be displayed for any logs
         errorMethodCount: 8, // Number of method calls if stacktrace is provided
         lineLength: 120, // Width of the output
-        colors: true, // Colorful log messages
+        colors: kDebugMode, // Colorful log messages
         printEmojis: true, // Print an emoji for each log message
         // Should each log print contain a timestamp
         dateTimeFormat: LoggerFactory.DateTimeFormat.dateAndTime,
@@ -43,16 +45,21 @@ class BaseLogger extends GetxService {
           LoggerFactory.Level.trace: false,
           LoggerFactory.Level.fatal: false,
         }
-      ), // Use the PrettyPrinter to format and print log
+      ),
       output: LoggerFactory.MultiOutput([
         DebugConsoleOutput(),
         LoggerFactory.AdvancedFileOutput(
           path: logDir,
           maxFileSizeKB: 1024 * 10,  // 10MB
           maxRotatedFilesCount: 5,
-          maxDelay: const Duration(seconds: 3)
+          maxDelay: const Duration(seconds: 3),
+          latestFileName: 'bluebubbles-latest.log',
+          fileNameFormatter: (timestamp) {
+            final now = DateTime.now();
+            return 'bluebubbles-${now.toIso8601String().split('T').first}-${now.millisecondsSinceEpoch ~/ 1000}.log';
+          },
         )
-      ]), // Use the default LogOutput (-> send everything to console)
+      ]),
     );
   }
 
@@ -64,44 +71,24 @@ class BaseLogger extends GetxService {
     logFilter = LoggerFactory.DevelopmentFilter();
   }
 
-  Future<void> writeLogToFile() async {
-    // // Create the log file and write to it
-    // if (kIsWeb) {
-    //   final bytes = utf8.encode(logs.join('\n'));
-    //   final content = base64.encode(bytes);
-    //   html.AnchorElement(href: "data:application/octet-stream;charset=utf-16le;base64,$content")
-    //     ..setAttribute("download", basename(logPath))
-    //     ..click();
-    //   return;
-    // }
-    // String filePath = logPath;
-    // if (kIsDesktop) {
-    //   filePath = fs.appDocDir.path;
-    //   DateTime now = DateTime.now().toLocal();
-    //   filePath = join(filePath, "Saved Logs",
-    //       "BlueBubbles_Logs_${now.year}-${now.month}-${now.day}_${now.hour}-${now.minute}-${now.second}.txt");
-    // }
-    // File file = File(filePath);
-    // file.createSync(recursive: true);
-    // file.writeAsStringSync(logs.join('\n'));
+  String compressLogs() {
+    final Directory logDir = Directory(Logger.logDir);
+    final date = DateTime.now().toIso8601String().split('T').first;
+    final File zippedLogFile = File("${fs.appDocDir.path}/bluebubbles-logs-$date.zip");
+    if (zippedLogFile.existsSync()) zippedLogFile.deleteSync();
 
-    // // Show the snackbar when finished
-    // showSnackbar(
-    //   "Success",
-    //   "Logs exported successfully${kIsDesktop ? "" : " to $filePath"}",
-    //   durationMs: 2500,
-    //   button: kIsDesktop || kIsWeb
-    //       ? null
-    //       : TextButton(
-    //           style: TextButton.styleFrom(
-    //             backgroundColor: Get.theme.colorScheme.surfaceVariant,
-    //           ),
-    //           onPressed: () {
-    //             Share.file("BlueBubbles Logs", filePath);
-    //           },
-    //           child: Text("SHARE", style: TextStyle(color: Get.theme.colorScheme.onSurfaceVariant)),
-    //         ),
-    // );
+    final List<FileSystemEntity> files = logDir.listSync();
+    final List<FileSystemEntity> logFiles = files.where((file) => file.path.endsWith(".log")).toList();
+    final List<String> logPaths = logFiles.map((file) => file.path).toList();
+
+    final encoder = ZipFileEncoder();
+    encoder.create(zippedLogFile.path);
+    for (final logPath in logPaths) {
+      encoder.addFile(File(logPath));
+    }
+    encoder.close();
+
+    return zippedLogFile.path;
   }
 
   void info(dynamic log, {String? tag, Object? error, StackTrace? trace}) => logger.i("[${tag ?? "BlueBubblesApp"}] $log", error: error, stackTrace: trace);
--- lib/utils/logger/outputs/debug_console_output.dart
+++ lib/utils/logger/outputs/debug_console_output.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
@@ -8,6 +10,36 @@ class DebugConsoleOutput extends LogOutput {
     // For "boxed" logs, print them as-is.
     // For not-boxed logs, print them as a single line.
     if ([Level.fatal, Level.error, Level.trace].contains(event.level)) {
+      StackTrace? trace = event.origin.stackTrace;
+      bool hasTrace = trace != null && trace.toString().isNotEmpty;
+      if (!hasTrace) {
+        trace = StackTrace.current;
+      }
+
+      String traceStr = trace.toString();
+
+      // Only take the last 3 lines of the trace
+      if (traceStr.trim().isNotEmpty) {
+        // Color logs red in debug mode
+        const colorPrefix = (kDebugMode) ? '\x1B[38;5;196m' : '';
+
+        // If there is no trace, (so we are using the current trace),
+        // we need to omit the first 5 lines of the trace, as they are
+        // part of the logger itself.
+        List<String> traceLines = traceStr.split('\n');
+        if (!hasTrace) {
+          traceLines = traceLines.sublist(5);
+        }
+
+        traceLines = (traceLines.length > 4) ? traceLines.sublist(0, 4) : traceLines;
+        traceStr = traceLines.map((e) => '$colorPrefix| $e').join('\n');
+
+        // Insert the trace into the box
+        event.lines.insert(event.lines.length - 1, '$colorPrefix|');
+        event.lines.insert(event.lines.length - 1, '$colorPrefix| Traceback:');
+        event.lines.insert(event.lines.length - 1, '$colorPrefix$traceStr');
+      }
+
       return event.lines.forEach(debugPrint);
     }
 
