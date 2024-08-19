5ce2dbed62e2c2ba82e87192043367a4bbbdfd33
zlshames <zlshames@gmail.com>
2024-08-19 12:52:03 -0400

    adds artificial delay to fix popscope issue for opening via a notif

--- lib/services/backend/java_dart_interop/intents_service.dart
+++ lib/services/backend/java_dart_interop/intents_service.dart
@@ -218,6 +218,7 @@ class IntentsService extends GetxService {
       if (!chatIsOpen) {
         Logger.debug("Navigating to conversation view...", tag: "IntentsService");
         await StartupTasks.waitForUI();
+        await Future.delayed(const Duration(milliseconds: 500));
         await ns.pushAndRemoveUntil(
           Get.context!,
           ConversationView(
