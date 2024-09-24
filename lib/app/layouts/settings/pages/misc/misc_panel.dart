2383072c7dc6f9c88bdb355da2159bcd27321ec9
zlshames <zlshames@gmail.com>
2024-09-24 08:25:39 -0400

    replace background service with foreground service nomenclature

--- lib/app/layouts/settings/pages/misc/misc_panel.dart
+++ lib/app/layouts/settings/pages/misc/misc_panel.dart
@@ -174,7 +174,7 @@ class _MiscPanelState extends OptimizedState<MiscPanel> {
                               }
                             },
                             initialVal: ss.settings.keepAppAlive.value,
-                            title: "Use Background Service For Notifications",
+                            title: "Use Foreground Service For Notifications",
                             subtitle:
                                 "Keep an always-open socket connection to the server for notifications, instead of registering with Firebase Cloud Messaging.",
                             isThreeLine: true,
