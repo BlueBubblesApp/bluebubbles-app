6ce622e18722c253a422378c762ce34e09da98aa
zlshames <zlshames@gmail.com>
2024-11-05 08:13:48 -0500

    separate incremental sync conditions for desktop vs android

--- lib/helpers/network/network_tasks.dart
+++ lib/helpers/network/network_tasks.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:bluebubbles/helpers/helpers.dart';
@@ -15,9 +16,20 @@ import 'package:network_tools/network_tools.dart'
 class NetworkTasks {
   static Future<void> onConnect() async {
     if (ss.settings.finishedSetup.value) {
-      // Only start incremental sync if the app is active and
-      // the previous state wasn't just hidden
-      // or if the app was never resumed before
+      
+      // Separate functionality for android vs. other
+      if (!Platform.isAndroid) {
+        // Only start incremental sync if the app is active and the previous state wasn't just hidden
+        // or if the app was never resumed before
+        if (ls.resumeFromPause == null || (ls.currentState == AppLifecycleState.resumed && ls.resumeFromPause!)) {
+          await sync.startIncrementalSync();
+        }
+      } else {
+        if (ls.currentState == AppLifecycleState.resumed && (ls.resumeFromPause == null || ls.resumeFromPause == true)) {
+          await sync.startIncrementalSync();
+        }
+      }
+
       if (ls.resumeFromPause == null || (ls.currentState == AppLifecycleState.resumed && ls.resumeFromPause!)) {
         await sync.startIncrementalSync();
       } else {
