2c1f0776bd298168adccefa511ac8bb5a28abfbb
Michael Rooplall <MichaelRooplall@gmail.com>
2026-07-30 08:05:03 -0400

    fix: guard reply/thread paths when a reply targets a missing message part

--- lib/models/message_reply_context.dart
+++ lib/models/message_reply_context.dart
@@ -5,7 +5,7 @@ import 'package:bluebubbles/database/models.dart';
 class MessageReplyContext {
   final Message message;
 
-  /// Message-part id being replied to (not a collapsed list index).
+  /// Message-part id being replied to
   final int partIndex;
 
   /// When set, scopes the reply to a specific attachment within [partIndex].
