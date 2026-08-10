da67fd5fd9a3088db17141489a5b457bee509a7d
Joel Jothiprakasam <hijoelj@gmail.com>
2026-08-10 11:42:05 -0700

    Revert "Fix unselected settings page background color"
    This reverts commit 05fd6ce1eaff5d320704a30a69905aa451aedad9.

--- lib/app/layouts/settings/settings_page.dart
+++ lib/app/layouts/settings/settings_page.dart
@@ -256,7 +256,8 @@ class _SettingsPageState extends State<SettingsPage> with ThemeHelpers {
                           CupertinoPage(
                               name: "initial",
                               child: Scaffold(
-                                  backgroundColor: Colors.transparent,
+                                  backgroundColor:
+                                      SettingsSvc.settings.skin.value != Skins.iOS ? tileColor : headerColor,
                                   body: Center(
                                     child: Text("Select a settings page from the list",
                                         style: context.theme.textTheme.bodyLarge),
